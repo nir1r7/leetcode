05d5a9c7b6000d047583d04ad00b0988cbc68195
Nirvan <nir1r7@users.noreply.github.com>
2025-09-16 11:18:52 -0400

    Can Place Flowers

---
+++ Can Place Flowers/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int len = flowerbed.length;
+        
+        for (int i = 0; i < len; i++){
+            if (flowerbed[i] != 1 && ((i-1 < 0 || (i-1 >= 0 && flowerbed[i-1] != 1)) && (i+1 >= len || (i+1 < len && flowerbed[i+1] != 1)))){
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+
+        if (n <= 0) return true;
+        return false;
+    }
+}
