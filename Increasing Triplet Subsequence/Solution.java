3b3e3005f768679ab16fcbfb01b1fe345bdcf5bc
Nirvan <nir1r7@users.noreply.github.com>
2025-09-19 16:21:46 -0400

    Increasing Triplet Subsequence

---
+++ Increasing Triplet Subsequence/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int n = nums.length;
+        
+        int num1 = Integer.MAX_VALUE;
+        int num2 = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++){
+            if (nums[i] <= num1) num1 = nums[i];
+            else if (nums[i] <= num2) num2 = nums[i];
+            else return true;
+        }
+        
+        return false;
+    }
+}
