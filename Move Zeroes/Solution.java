e3d914c7cebf7ce8f7e0881080a3d14f5365e86f
Nirvan <nir1r7@users.noreply.github.com>
2025-09-22 16:54:35 -0400

    Move Zeroes

---
+++ Move Zeroes/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int p1 = 0;
+
+        for (int i = 0; i < n; i++){
+            if (nums[i] != 0){
+                nums[p1++] = nums[i];
+            }
+        }
+        for (int i = p1; i < n; i++){
+            nums[i] = 0;
+        }
+
+    }
+}
